eee02495cdbfaac40bc2037ac61438306ca9c54d
harshmankoo <ravkiratsingh9988@gmail.com>
2026-03-11 16:55:32 +0530

    Done First React Task via Vite

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Footer from './components/Footer'
+
+function App() {
+
+
+  return (
+    <>
+     <h1>Hey This is my First React Task</h1>
+     <h1>My Self Harshdeep Singh</h1>
+  <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ my-react-app/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return(
+        <>
+    <h1>Hey This is a Footer</h1>
+        </>
+    )
+}
+export default Footer
