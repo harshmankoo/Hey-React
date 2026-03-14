e9da67ad94ea8262efb9efd627f981156768c59b
harshmankoo <ravkiratsingh9988@gmail.com>
2026-03-14 17:10:41 +0530

    updated navbar task

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -13,15 +13,17 @@ function App() {
 
   return (
     <>
+     <Navbar />
+     <br />
+  <Profile />
     {/* /* <div className='div1'> <h1>Hey This is my First React Task</h1></div>
 
-    
+   
   <Header />
   <Greetings />
   <Button />
   <Card />  */}
-  <Navbar />
-  <Profile />
+  
   
     </>
   )
--- my-react-app/src/components/Navbar.jsx
+++ my-react-app/src/components/Navbar.jsx
@@ -1,19 +1,20 @@
 import "./Navbar.css"
 
-const  Navbar = ()=>{
+const Navbar = () => {
     return (
-     <nav className="header">
-        <div className="Navlogo">
-        </div>
-        <div className="SearchBar"><input type="text" placeholder="Search...." />
-        </div>
+        <nav className="header">
+            <div className="Navlogo">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQ2qA2C7_fU5hAhs04cKig1LoENHMDNw4pCw&s" alt="Logo Image" />
+            </div>
+            <div className="SearchBar"><input type="text" placeholder="Search...." />
+            </div>
 
-        <div className="UserIcon">
-            <i class="fa-solid fa-user"></i>
-        </div>
-        
-     </nav>
-        
+            <div className="UserIcon">
+                <i className="fa-solid fa-user"></i>
+            </div>
+
+        </nav>
+
     )
 }
 export default Navbar
