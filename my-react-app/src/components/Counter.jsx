1ab59dfdaddbde7a052b2fd3ff905c329bb4288a
harshmankoo <ravkiratsingh9988@gmail.com>
2026-03-18 18:33:34 +0530

    use of usestate in array rendering

--- my-react-app/src/components/Counter.jsx
+++ my-react-app/src/components/Counter.jsx
@@ -4,7 +4,16 @@ import { useState } from "react"
 function Counter() {
     const [count, setcount] = useState(0)
     const [name, setName] = useState("")
-    const[show,setShow]= useState(true)
+    const [show, setShow] = useState(true)
+    const [showMassage, setshow] = useState(false)
+    const [showData, setUsers] = useState([])
+    const users = ["Dev", "Prem", "Raj"]
+    const products = [
+        { name: "Shoes", price: 800 },
+        { name: "Shirt", price: 400 },
+        { name: "Watch", price: 1200 },
+        { name: "Cap", price: 200 }
+    ];
 
     const handleChange = (e) => {
         console.log(e.target.value)
@@ -16,14 +25,10 @@ function Counter() {
         alert("Submitted by " + name)
     }
 
+    const addData = () => {
+        setUsers([...showData, "New list"])
+    }
 
-//   let message;
-
-//   if (show) {
-//     message = <h1>Hello World !!</h1>;
-//   } else {
-//     message = null;
-//   }
 
     return (
         <>
@@ -32,20 +37,73 @@ function Counter() {
             <button onClick={() => (setcount(count - 1))}>Decrement</button><br />
 
             <br />
-{/* ================================================================ */}
+            ================================================================ 
             <form onSubmit={handleSubmit}>
                 <input type="text" placeholder="Enter Your Name...." onChange={handleChange} />
                 <button type="submit">Submit</button>
             </form>
             <h2>Hello from {name}</h2>
-{/* ======================================================================= */}
+            =======================================================================
             <br />
-{show ? <h1>Hello World !!</h1> : null}
-<button onClick={()=>(setShow(!show))}>TOGGLE</button>
-{/* {message} */}
+            {show ? <h1>Hello World !!</h1> : null}
+            <button onClick={() => (setShow(!show))}>TOGGLE</button>
 
-{/* =========================================================================== */}
-        </>
+            <br />
+            <br />
+            ===========================================================================
+<br />
+
+            {/* Task 1
+
+Toggle text using button */}
+
+
+            {showMassage ? <h1>Hey i am using a usestate for the toggle Message </h1>
+                : null}
+
+            <button onClick={() => (setshow(!showMassage))}>Click Here to Reveal the Info</button>
+            <br />
+=====================================================================================
+<br />
+
+{/*
+Task 2
+
+Show “No Data” if list empty
+Otherwise show list */}
+
+
+<button onClick={addData}>Show List</button>
+{
+showData.length===0 ?(<p>NO DATA FOUND IN THE LIST</p>) : (
+    <ul>
+        { showData.map((x,i)=>(
+            <li key = {i}>{x}</li>
+        ))}
+    </ul>
+)}
+
+// ============================================================================================
+
+{/* this task is about the accesing aarray value without useState */}
+
+{/* i am here rendering the list */}
+<ul>
+    {
+        users.map((x,i)=>(
+            <li key = {i}> {x} </li>
+        ))
+    }
+</ul>
+
+<br />
+
+<br />
+===================================================================================================
+
+
+
+    </>
     )
 }
 
